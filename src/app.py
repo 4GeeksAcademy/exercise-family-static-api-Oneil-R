b4b6c75c494d7c46547ffeb1c9f006c1259c9597
Oneil-R <167453370+Oneil-R@users.noreply.github.com>
2024-08-23 22:58:01 +0000

    completed

--- src/app.py
+++ src/app.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
@@ -30,13 +27,44 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify (members) , 200
+    
 
 
-    return jsonify(response_body), 200
+   
+@app.route('/members', methods=['GET'])
+def handle_get_all_members():
+    try:
+        members = jackson_family.get_all_members()
+        return jsonify(members), 200
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 500
+
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_get_member(id):
+    try:
+        member = jackson_family.get_member(id)
+        if member:
+            return jsonify(member), 200
+        else:
+            return jsonify({"msg": "Member not found"}), 404
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 500
+
+
+@app.route('/member', methods=['POST'])
+def handle_add_member():
+    member = request.json
+    jackson_family.add_member(member)
+    return {}, 200
+    
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(id)
+    return {"done":True},200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,19 +1,56 @@
-
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
+# initial_family_data = [(
+#              {
+#                 "id": self._generateId(),
+#                 "first_name": "John",
+#                 "last_name": "Jackson",
+#                 "age": 33,
+#                 "lucky_numbers": [7, 13, 22]
+#             },
+#             {
+#                 "id": self._generateId(),
+#                 "first_name": "Jane",
+#                 "last_name": "Jackson",
+#                 "age": 35,
+#                 "lucky_numbers": [10, 14, 3]
+#             },
+#             {
+#                 "id": self._generateId(),
+#                 "first_name": "Jimmy",
+#                 "last_name": "Jackson",
+#                 "age": 5,
+#                 "lucky_numbers": [1]
+#             }
+#         )]
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+             {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": "Jackson",
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": "Jackson",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": "Jackson",
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,14 +58,23 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        self._members.append(member)
+        return True
 
     def delete_member(self, id):
         # fill this method and update the return
+        self._members = list(filter(lambda x: x["id"] != id,self._members))
         pass
 
+
+     
     def get_member(self, id):
         # fill this method and update the return
+        members = list(filter(lambda x: x["id"] == id,self._members))
+        if len(members) > 0 and members is not None:
+            return members[0]
+        else:
+            return None
         pass
 
     # this method is done, it returns a list with all the family members
